e91ecd70a8cd8fca547e6f24c8918507c7938038
Devon <devonhamill93@gmail.com>
2024-05-09 12:56:10 -0400

    Initial header and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Layout from './Layout';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/" exact render={() => <h1>Hello world</h1>} />
+          <Route render={() => <h1>404 Not Found</h1>} />
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
